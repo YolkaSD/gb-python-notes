cc021b941d548df72bd7ab5e40e7f223e36938fb
yolkaSD <yolka.sd@gmail.com>
2023-08-07 21:25:14 +0700

    created filter by date

--- main.py
+++ main.py
@@ -55,6 +55,30 @@ def print_notes(notes):
         print("-" * 30)
 
 
+def print_note_by_id(notes):
+    note_id = input("Enter note ID to view: ")
+    if note_id in notes:
+        note_info = notes[note_id]
+        print(f"Note ID: {note_id}")
+        print(f"Title: {note_info['title']}")
+        print(f"Body: {note_info['body']}")
+        print(f"Date/Time: {note_info['date_time']}")
+    else:
+        print("Note not found!")
+
+
+def filter_notes_by_date(notes):
+    date_str = input("Enter date in format (YYYY-MM-DD): ")
+    try:
+        date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+        filtered_notes = {note_id: note_info for note_id, note_info in notes.items() if
+                          note_info['date_time'].startswith(date_str)}
+        return filtered_notes
+    except ValueError:
+        print("Invalid date format. Use YYYY-MM-DD.")
+        return {}
+
+
 def main():
     filename = 'notes.json'
     notes = load_notes(filename)
@@ -64,8 +88,10 @@ def main():
         print("1. Add Note")
         print("2. Edit Note")
         print("3. Delete Note")
-        print("4. Print Notes")
-        print("5. Save and Quit")
+        print("4. View Note by ID")
+        print("5. Filter Notes by Date")
+        print("6. Print All Notes")
+        print("7. Save and Quit")
         choice = input("Enter your choice: ")
 
         if choice == '1':
@@ -75,8 +101,13 @@ def main():
         elif choice == '3':
             delete_note(notes)
         elif choice == '4':
-            print_notes(notes)
+            print_note_by_id(notes)
         elif choice == '5':
+            filtered_notes = filter_notes_by_date(notes)
+            print_notes(filtered_notes)
+        elif choice == '6':
+            print_notes(notes)
+        elif choice == '7':
             save_notes(notes, filename)
             print("Notes saved. Exiting...")
             break
